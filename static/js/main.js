604909f9a8288793eb4c3e14873205ab8e175756
Christian West <christianwest@cedub.local>
2013-10-04 16:03:56 -0600

    Added animations

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
+var HEIGHT = $(window).height(),
+WIDTH = $(window).width();
+
 var stage = new Kinetic.Stage({
   container: 'container',
-  width: $(window).width(),
-  height: $(window).height()
+  width: WIDTH,
+  height: HEIGHT
 });
 var layer = new Kinetic.Layer();
 
@@ -10,15 +13,29 @@ var layer = new Kinetic.Layer();
  ** Add a Landscape **
  *********************/
 
-  var redLine = new Kinetic.Line({
-    points: [[0,0], [$(window).width(),0]],
-    stroke: 'black',
-    strokeWidth: 5,
+var bgImg = new Image();
+bgImg.onLoad = function () {
+  var bridge = new Kinetic.Image({
     x: 0,
-    y: 300
+    y: 100,
+    image: bgImg,
+    width: 3560,
+    height: 756
   });
+  layer.add(bridge);
+  console.log("loaded");
+};
+bgImg.src = 'static/img/bridge-back-1.png';
 
-  layer.add(redLine);
+var line = new Kinetic.Line({
+  points: [[0,0], [WIDTH,0]],
+  stroke: 'black',
+  strokeWidth: 5,
+  x: 0,
+  y: 300
+});
+
+layer.add(line);
 
 
 /**********************
@@ -42,7 +59,7 @@ var imageObj = new Image();
 imageObj.src = 'static/img/kidneyDanceSmall.png';
 
 var blob = new Kinetic.Sprite({
-  x: 250,
+  x: -300,
   y: 200,
   image: imageObj,
   animation: 'idle',
@@ -59,6 +76,168 @@ layer.add(blob);
 blob.start();
 
 
+/**************************
+ ** Character Animation **
+ *************************/
+
+var idleAnimation = [{
+    x: 0,
+    y: 3,
+    width: 300,
+    height: 291
+}],
+jumpAnimation = [{
+    x: 0,
+    y: 3,
+    width: 300,
+    height: 291
+  },{
+    x: 0,
+    y: 293,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 583,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 873,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 1163,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 1453,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 1743,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 2033,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 2323,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 2613,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 2903,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 3193,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 3483,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 3773,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 4063,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 4353,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 4643,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 4933,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 5223,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 5513,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 5803,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 6673,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 6963,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 7253,
+    width: 300,
+    height: 290
+  },{
+    x: 0,
+    y: 7543,
+    width: 300,
+    height: 290
+  }];
+var charAnimations = {
+  idle: idleAnimation,
+  jump: jumpAnimation
+};
+
+var charObj = new Image();
+charObj.src = 'static/img/jump.png';
+
+var charBlob = new Kinetic.Sprite({
+  x: 290,
+  y: 200,
+  image: charObj,
+  animation: 'idle',
+  animations: charAnimations,
+  frameRate: 30,
+  index: 0,
+  draggable: true
+});
+
+// add the shape to the layer
+layer.add(charBlob);
+
+// start sprite animation
+charBlob.start();
+
+
 /************************
  ** Add layer to stage **
  ************************/
@@ -71,8 +250,62 @@ stage.add(layer);
  ** Move Character **
  ********************/
 
+var dir = 'right';
 var move_anim = new Kinetic.Animation(function (frame) {
-  blob.setX(blob.getX()+5);
+  if (blob.getX() >=  WIDTH/2) {
+    this.stop();
+  }
+  if (dir == 'right') {
+    blob.setX(blob.getX()+2);
+  } else {
+    blob.setX(blob.getX()-2);
+  }
 }, layer);
 
 move_anim.start();
+
+
+/******************
+ ** Add Controls **
+ ******************/
+
+var char_dir = 'right';
+var move_char_anim = new Kinetic.Animation(function (frame) {
+  if (charBlob.getX() > WIDTH-40 || charBlob.getX() <= 10) {
+    this.stop();
+  }
+  if (char_dir == 'right') {
+    charBlob.setX(charBlob.getX()+2);
+  } else {
+    charBlob.setX(charBlob.getX()-2);
+  }
+}, layer);
+
+
+$(document).on('keydown', function (e) {
+  e.preventDefault();
+  switch (e.keyCode) {
+    case 40:
+      alert('down');
+      break;
+    case 38:
+      charBlob.setAnimation('jump');
+      charBlob.afterFrame(24, function () {
+        charBlob.setAnimation('idle');
+      })
+      console.log('up');
+      break;
+    case 37:
+      char_dir = 'left';
+      move_char_anim.start();
+      console.log('left');
+      break;
+    case 39:
+      char_dir = 'right';
+      move_char_anim.start();
+      console.log('right');
+      break;
+  }
+});
+
+
